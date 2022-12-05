18c32a8110ba2162eda8dfb57d80861694a4c04c
Amaral220x <biel.ilha2021@gmail.com>
2022-12-05 20:17:28 -0300

    dropzin kk

--- 2737.sql
+++ 2737.sql
@@ -23,3 +23,5 @@ UNION ALL
 SELECT name,customers_number FROM lawyers WHERE customers_number = (SELECT MIN(customers_number) FROM lawyers)
 UNION ALL
 SELECT 'Average', round(AVG(customers_number), 0) FROM lawyers;
+
+DROP TABLE lawyers;
